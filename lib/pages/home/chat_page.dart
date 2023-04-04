1ca7d64e4413bb505cc4331b89216c8e6d5bf659
riskiai <riskiahmadilham2002@gmail.com>
2023-04-04 21:33:03 +0700

    masih revisi

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,14 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:proyek3/theme.dart';
 
+
 class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: primaryTextStyle,
-      ),
+    /* Membuat Komponen Header */
+    Widget header() {
+      return AppBar(
+        backgroundColor: bg3greenColor,
+        centerTitle: true,
+        title: Text(
+          'Konsultasi Produk',
+          style: putihTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    /* JIKA KONDISI CHAT MASIH KOSONG */
+    Widget emptyChat() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: bggreenColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icon_headset.png',
+                width: 150,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Konsultasikan Kendala Anda',
+                style: putihTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'Terimakasih Sudah Memesan Produk Kami',
+                style: tulisanRP,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 27,
+                      vertical: 5,
+                    ),
+                    backgroundColor: bg2greenColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Kunjungi Toko',
+                    style: putihTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+        width: double.infinity,
+        color: bggreenColor,
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            ChatTile(),
+          ],
+        ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+        // emptyChat(),
+      ],
     );
   }
 }
---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:proyek3/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  const ChatTile({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail-chat');
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 33),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/image_shop_logo.png',
+                  width: 54,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Store Asap Cair',
+                        style: putihTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        'Ada Yang Bisa Kami Bantu ? Yuk Konsultasikan Permasalahan Kamu Dengan Obat Asap Cair Semoga Membantu',
+                        style: ijo1TextStyle.copyWith(fontWeight: light),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Active',
+                  style: putihTextStyle.copyWith(
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Divider(
+              thickness: 1,
+              color: Color(0xff7ED957),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
